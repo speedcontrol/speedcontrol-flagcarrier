f48bb4abdf25d01fc9c358701ec8bf88f8a65a80
BtbN <btbn@btbn.de>
2019-07-20 14:34:30 +0200

    Implement support for simultanous login_clear

--- extension/index.js
+++ extension/index.js
@@ -21,8 +21,20 @@ module.exports = function(nodecg) {
 				return;
 		}
 
+		// Request to clear the relevant data from our storage.
+		if (req.body.action === 'clear' || req.body.action === 'login_clear') {
+			// Completely deletes the group's object.
+			delete users.value[req.body.group_id];
+
+			// Also send the request in a message.
+			nodecg.sendMessage('clear', req.body);
+
+			nodecg.log.info('The %s group was cleared using FlagCarrier app (DeviceID: %s).', req.body.group_id, req.body.device_id);
+			res.send('All users from this group have been cleared.');
+		}
+
 		// Store the data in our storage.
-		if (req.body.action === 'login') {
+		if (req.body.action === 'login' || req.body.action === 'login_clear') {
 			// Create object for the group if needed.
 			if (!users.value[req.body.group_id])
 				users.value[req.body.group_id] = {};
@@ -35,18 +47,6 @@ module.exports = function(nodecg) {
 
 			nodecg.log.info('%s logged in using FlagCarrier app (Group: %s, Position: %s, DeviceID: %s).', req.body.tag_data.display_name, req.body.group_id, req.body.position, req.body.device_id);
 			res.send('You\'ve been logged in.');
-		}
-
-		// Request to clear the relevant data from our storage.
-		else if (req.body.action === 'clear') {
-			// Completely deletes the group's object.
-			delete users.value[req.body.group_id];
-
-			// Also send the request in a message.
-			nodecg.sendMessage('clear', req.body);
-
-			nodecg.log.info('The %s group was cleared using FlagCarrier app (DeviceID: %s).', req.body.group_id, req.body.device_id);
-			res.send('All users from this group have been cleared.');
 		}
 	});
 	nodecg.mount(app);
